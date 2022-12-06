b60456f8fb2eff8e1533f6c670ab1ba8dba1baa1
Art <vitaminearte@gmail.com>
2022-12-06 18:04:43 +0200

    loading first screen, fixed random positioning for friends grid

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,18 +1,25 @@
+const cards = Array.from(document.querySelectorAll('.friends-grid .card'));
+
+const randomCards = cards.sort(randomizeFunc);
+
+function randomizeFunc(a, b) {
+  return 0.5 - Math.random();
+}
+
+document.querySelector('.friends-grid').innerHTML = '';
+randomCards.forEach((card, index)=>{
+    document.querySelector('.friends-grid').appendChild(card)
+    card.classList.add(`card-${index+1}`)
+})
+
 gsap.registerPlugin(ScrollTrigger);
-// ScrollTrigger.create({
-//     trigger: '#dropcap',
-//     start: 'bottom 100%',
-//     markers: true,
-//     toggleActions: 'restart none reverse none',
-//     toggleClass: 'animate__wobble'
-// })
 
 ScrollTrigger.matchMedia({
     "(max-width:899px)": function () {
         const boxes = gsap.utils.toArray('.card');
 
         boxes.forEach((box, i) => {
-        const anim = gsap.from(box, {opacity: 0, y: 50, scale:.8, duration: .6, delay: i * 0.1, ease: 'circ.Out()'});
+        const anim = gsap.from(box, {opacity: 0, y: 50, scale:.8, duration: .6, delay: i * 0.07, ease: 'circ.Out()'});
             ScrollTrigger.create({
                 trigger: box,
                 animation: anim,
@@ -41,15 +48,36 @@ ScrollTrigger.matchMedia({
         })
     }
 })
-gsap.from(".hero-bg-planet", {
+gsap.fromTo(".hero-bg-planet", {
     scrollTrigger: {
         trigger: '.hero-bg-planet',
     },
     opacity: 0,
     scale: 0,
     filter: 'blur(5px)',
-    rotate: 20,
+    rotate: 0,
+}, {
+    opacity: 1,
+    scale: 1.5,
+    filter: 'none',
+    rotate: -20,
     duration: 5,
     ease: 'expo.out()'
+
 })
+gsap.fromTo(".friends-collage", {
+    scrollTrigger: {
+        trigger: '.friends-collage',
+    },
+    opacity: 0,
+    scale: 0,
+}, {
+    opacity: 1,
+    scale: 1,
+    duration: 3,
+    ease: 'expo.out()'
+
+}
+)
+
 
